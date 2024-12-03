981df7d9ed2d6674490d35214a3113a725cc6f25
antonioclp <antonionetosg6@gmail.com>
2024-12-03 15:41:46 -0300

    delete src

--- src/utils/validations/index.ts
+++
@@ -1,9 +0,0 @@
-export function emailValidation(email: string) {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  return emailRegex.test(email)
-}
-
-export function passwordValidation(password: string) {
-  const passwordRegex = /^.{6,}$/
-  return passwordRegex.test(password)
-}
